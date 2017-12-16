eea1b3545d00917c97c3f74a1119c4a67446bc37
nela-dj <marinelaparovic@gmail.com>
2017-12-16 15:57:31 +0100

    Added white vertical lines to separate black squares.

--- ms_main.hs
+++ ms_main.hs
@@ -25,16 +25,20 @@ background = cyan
 --draw_rect :: Float -> Float -> Float -> Float -> Picture
 draw_rect x_begin x_end y_begin y_end = polygon [(x_begin, y_begin), (x_end, y_begin), (x_end, y_end), (x_begin, y_end)]
 
-coordinates = [(-size) `div` 2, (-size) `div` 2 + size `div` fields_num ..]
+coordinates = map fromIntegral [(-size) `div` 2, (-size) `div` 2 + size `div` fields_num ..]
 
 begin_coordinates = take fields_num coordinates
 end_coordinates = take fields_num (tail coordinates)
 
-draw_rect_row y_begin y_end = map (\tuple_x -> draw_rect (fromIntegral $ fst tuple_x) (fromIntegral $ snd tuple_x) y_begin y_end) 
+draw_rect_row y_begin y_end = map (\tuple_x -> draw_rect (fst tuple_x) (snd tuple_x) y_begin y_end) 
  $ zip begin_coordinates $ end_coordinates  
 
-drawing = pictures $ concat $ map (\tuple_y -> draw_rect_row (fromIntegral $ fst tuple_y) (fromIntegral $ snd tuple_y)) 
- $ zip begin_coordinates $ end_coordinates
+v_lines = map (color white) $ map (\x -> line[(x, fromIntegral $ (-size) `div` 2),(x, fromIntegral $ size `div` 2)]) begin_coordinates
+h_lines = map (color white) $ map (\y -> line[(fromIntegral $ (-size) `div` 2, y),(fromIntegral $ size `div` 2, y)]) begin_coordinates
+
+
+drawing = pictures $ (concat $ map (\tuple_y -> draw_rect_row (fst tuple_y) (snd tuple_y)) 
+ $ zip begin_coordinates $ end_coordinates) ++ v_lines ++ h_lines
 
 
 main :: IO()
