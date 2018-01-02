eef96ff2f64a2897d336ece15999649a6928cf7d
mm11021 <mm11021@alas.matf.bg.ac.rs>
2018-01-02 15:17:54 +0100

    Izbacen ansejf. Slike ubacene u igricu u IO delu.

--- ms_main.hs
+++ ms_main.hs
@@ -5,62 +5,36 @@ import Graphics.Gloss.Interface.Pure.Game
 import System.Random
 import Data.Time.Clock.POSIX
 import Data.List
-import System.IO.Unsafe
+--import System.IO.Unsafe (nije vise potrebno)
 --import Graphics.Gloss.Game (kod Dimitrija nece da se kompajlira ako ima ovog)
 --import Data.List.Split
 
-
 --uzimamo trenutno vrijeme
+current_time :: IO Int
 current_time = fmap round getPOSIXTime
 
 --size, fields_num, mines_num ce biti stepeni broja 2
 --dimenzije prozora ce biti size x size 
-size = 64*fields_num
+field_size :: Int
+field_size = 64
+
+half_field_size :: Float
+half_field_size = (fromIntegral field_size) / 2
+
+size :: Int
+size = field_size*fields_num
 
 --pozicija prozora
+position :: Int
 position = 0
 
 --igra ce imate fields_num x fields_num polja
+fields_num :: Int
 fields_num = 8
 
 --broj mina u igri je mines_num
+mines_num :: Int
 mines_num = (fields_num `div` 8) * (fields_num `div` 8) * 10
-
-
---funkcija koja vraca listu od n random brojeva i generator
---koristimo trenutno vrijeme za randomizaciju
---poziva se sa make_rs n (fmap mkStdGen current_time)
-make_rs :: RandomGen g => Int -> g -> ([Int],g)
-make_rs n g = loop [] n g
-  where
-  loop acc 0 g = (reverse acc,g)
-  loop acc n g = let (r,g') = randomR (0,n) g 
-    in loop (r:acc) (pred n) g'
-
-
---sada u ovoj listi samo treba uzeti indekse nekog broja od 0 do 5 koji se pojavljuje bar mines_num puta
---i ti indeksi ce biti pozicije mina na osnovu kojih treba ispravno napraviti funkciju generateInitialState
-random_list = unsafePerformIO $ fmap (map (\x -> x `mod` 6)) $ fmap fst $ fmap (make_rs $ fields_num*fields_num) (fmap mkStdGen current_time)
-
---sljedeca linija pronalazi najmanji element od 0 do 5 sa frekvencijom bar mines_num, indeksi u random_list tog elementa bice pozicije mina
---treba pojednostaviti sljedeci kod
-selectedElem = ( fst . head . dropWhile (\g -> snd g < mines_num) . map (\g -> (head g, length g)). group . sort) random_list
-
---TODO
---Uraditi ovo sa foldl
-minesBefore :: Int -> [(Bool, Int)] -> [(Bool, Int)]
-minesBefore _ []     = []
-minesBefore acc (x:xs) = if fst x == True then (fst x,acc+1+snd x) : minesBefore (acc+1) xs
-                                        else (fst x, acc) : minesBefore acc xs
-
-
-matrix_many_mines = map (\x -> x == selectedElem) random_list
-
-mines_matrix = let minesBefore10 = minesBefore 0 $ zip matrix_many_mines (repeat 0)
-                   firstPart = map fst $ takeWhile (\x -> snd x < 10) $ minesBefore10
-                   lengthTaken = length firstPart
-               in take (lengthTaken+1) matrix_many_mines ++ take ((fields_num*fields_num)-lengthTaken-1) (repeat False)
-
 
 mainWindow :: Display
 mainWindow = InWindow "Minesweeper" (size,size) (position,position)
@@ -86,10 +60,6 @@ data GameState = Game ([FieldValue], [FieldState])
  | Win ([FieldValue], [FieldState])
  deriving Show
 
---ovde bi trebalo da budu: matrica kliknutih polja, matrica mina i mozda jos ponesto...
-initialState :: GameState
-initialState = generateInitialState fields_num mines_num
-
 -- ova funkcija ce na osnovu generisanih mina postaviti ispravne brojeve od 0 do 8 na sva ostala polja
 count_mines :: [FieldValue] -> [FieldValue]
 count_mines x =
@@ -101,19 +71,6 @@ count_mines x =
                             broj_mina = length $ filter (Mine ==) [snd $ polja !! (fields_num*a+b) | (a,b) <- indeksi_suseda]
                         in Neighbours broj_mina
   in map count polja
-
---funkcija koja na pocetku generise igru, ona treba da postavi mine, pa postavi brojeve od 0-8 u ostala polja
---ona ce da postavi sva polja matrice za stanje igre na Uncovered
-
-generateInitialState :: Int -> Int -> GameState
---generateInitialState fields_n mines_n = 
- --let fields_value = count_mines $ map (\x -> if (fst x) `mod` fields_n == 0 then Mine else Neighbours 0) $ zip [0,1..] $ take (fields_n*fields_n) $ map (\x -> Neighbours 0) [1..]
-     --fields_state = map (\x -> Uncovered) fields_value   
- --in Game (Data.List.Split.chunksOf fields_n fields_value,Data.List.Split.chunksOf fields_n fields_state)
-generateInitialState fields_n mines_n =
-   let fields_value = count_mines $ map (\x -> if fst x then Mine else Neighbours 0) $ zip mines_matrix [0..]
-       fields_state = map (\x -> Uncovered) fields_value 
-   in Game (fields_value, fields_state)
 
 --broj frejmova u sekundi
 fps :: Int
@@ -211,28 +168,12 @@ handleKeys (EventKey (MouseButton RightButton) Down _ (x,y)) game = flag x y gam
 --na ostale dogadjaje ne reaguje
 handleKeys _ game = game
 
---slike :: [Picture]
---slike = map (\x -> loadBMP $ "76px-Minesweeper_" ++ show x ++ "_svg.bmp") [0,1..8]
-
---mina :: Picture
---mina = loadBMP "1473847111_large.bmp"
-
---zastavica :: Picture
---zastavica = loadBMP "76px-Minesweeper_flag_svg.bmp"
-
---precrtano :: Picture
---precrtano = loadBMP "1473847111_large_crossed.bmp"
-
---IMPLEMENTIRATI
-draw_rect :: (FieldValue, FieldState) -> Float -> Float -> Float -> Float -> Picture
-draw_rect (value, Uncovered) x_begin x_end y_begin y_end = polygon [(x_begin, y_begin), (x_end, y_begin), (x_end, y_end), (x_begin, y_end)]
-draw_rect (value, Clicked Mine) x_begin x_end y_begin y_end = translate (x_begin + 7.5) (y_begin + 10) $ scale 0.6 0.4 (Text "M")
-draw_rect (value, Clicked (Neighbours x)) x_begin x_end y_begin y_end = 
- if x == 0
- then Text "" 
- else translate (x_begin + 7.5) (y_begin + 10) $ scale 0.6 0.4 (Text (show x))
- 
-draw_rect (value, Flag) x_begin x_end y_begin y_end = translate (x_begin + 7.5) (y_begin + 10) $ scale 0.6 0.4 (Text "F")
+draw_rect :: [Picture] -> (FieldValue, FieldState) -> Float -> Float -> Float -> Float -> Picture
+draw_rect pics (value, Uncovered) x_begin x_end y_begin y_end = polygon [(x_begin, y_begin), (x_end, y_begin), (x_end, y_end), (x_begin, y_end)]
+draw_rect pics (value, Clicked Mine) x_begin x_end y_begin y_end = translate (x_begin + half_field_size) (y_begin + half_field_size) $ scale 0.875 0.875 $ pics !! 9
+draw_rect pics (value, Clicked (Neighbours x)) x_begin x_end y_begin y_end = translate (x_begin + half_field_size) (y_begin + half_field_size) $ scale 0.875 0.875 $ pics !! x
+draw_rect pics (value, Flag) x_begin x_end y_begin y_end = translate (x_begin + half_field_size) (y_begin + half_field_size) $ scale 0.875 0.875 $ pics !! 10
+draw_rect pics (value, FalseFlag) x_begin x_end y_begin y_end = translate (x_begin + half_field_size) (y_begin + half_field_size) $ scale 0.875 0.875 $ pics !! 11
 
 coordinates = map fromIntegral [(-size) `div` 2, (-size) `div` 2 + size `div` fields_num ..]
 begin_coordinates = take fields_num coordinates
@@ -250,8 +191,8 @@ column_num y = (fromIntegral $ fields_num-1) - (row_num y)
 takeElemValueState :: [FieldValue] -> [FieldState] -> Float -> (FieldValue, FieldState)
 takeElemValueState values states col_num = (values !! (round col_num), states !! (round col_num))
 
-draw_rect_row :: ([FieldValue], [FieldState]) -> Float -> Float -> [Picture]
-draw_rect_row pair_game y_begin y_end = map (\tuple_x -> draw_rect (takeElemValueState (fst pair_game) (snd pair_game) (column_num $ snd   tuple_x)) (fst tuple_x) (snd tuple_x) y_begin y_end) $ zip begin_coordinates end_coordinates  
+draw_rect_row :: [Picture] -> ([FieldValue], [FieldState]) -> Float -> Float -> [Picture]
+draw_rect_row pics pair_game y_begin y_end = map (\tuple_x -> draw_rect pics (takeElemValueState (fst pair_game) (snd pair_game) (column_num $ snd   tuple_x)) (fst tuple_x) (snd tuple_x) y_begin y_end) $ zip begin_coordinates end_coordinates  
 
 v_lines = map (color white) $ map (\x -> line[(x, fromIntegral $ (-size) `div` 2),(x, fromIntegral $ size `div` 2)]) begin_coordinates
 h_lines = map (color white) $ map (\y -> line[(fromIntegral $ (-size) `div` 2, y),(fromIntegral $ size `div` 2, y)]) begin_coordinates
@@ -266,37 +207,98 @@ takeRowValuesStates values states row_number =
  in (row_values, row_states)
 
 --funkcija render sluzi da trenutno stanje igre pretvori u sliku koja ce nam se prikazati
-render :: GameState -> Picture
+--kao prvi argument prima niz slika koje ce se prikazivati, a koje se ucitaju u IO delu programa, a kao drugi trenutno stanje igre
+render :: [Picture] -> GameState -> Picture
 
-render (Game pair_game) = 
+render pics (Game pair_game) = 
  let values = fst pair_game
      states = snd pair_game
- in pictures $ (concat $ map (\tuple_y -> draw_rect_row (takeRowValuesStates values states $ row_num $ snd tuple_y) (fst tuple_y) (snd tuple_y)) 
+ in pictures $ (concat $ map (\tuple_y -> draw_rect_row pics (takeRowValuesStates values states $ row_num $ snd tuple_y) (fst tuple_y) (snd tuple_y)) 
   $ zip begin_coordinates end_coordinates)
   ++ v_lines 
   ++ h_lines
 
-render (GameOver pair_game) = 
+render pics (GameOver pair_game) = 
  let values = fst pair_game
      states = snd pair_game
- in pictures $ (concat $ map (\tuple_y -> draw_rect_row (takeRowValuesStates values states $ row_num $ snd tuple_y) (fst tuple_y) (snd tuple_y)) 
+ in pictures $ (concat $ map (\tuple_y -> draw_rect_row pics (takeRowValuesStates values states $ row_num $ snd tuple_y) (fst tuple_y) (snd tuple_y)) 
   $ zip begin_coordinates end_coordinates)
   ++ v_lines 
   ++ h_lines
 
-render (Win pair_game) = 
+render pics (Win pair_game) = 
  let values = fst pair_game
      states = snd pair_game
- in pictures $ (concat $ map (\tuple_y -> draw_rect_row (takeRowValuesStates values states $ row_num $ snd tuple_y) (fst tuple_y) (snd tuple_y)) 
+ in pictures $ (concat $ map (\tuple_y -> draw_rect_row pics (takeRowValuesStates values states $ row_num $ snd tuple_y) (fst tuple_y) (snd tuple_y)) 
   $ zip begin_coordinates end_coordinates)
   ++ v_lines 
   ++ h_lines
 
 main :: IO()
-main = play mainWindow background fps initialState render handleKeys update
-  where
-    update :: Float -> GameState -> GameState
-    update _ = id
+main = do
+         --sada u ovoj listi samo treba uzeti indekse nekog broja od 0 do 5 koji se pojavljuje bar mines_num puta
+         --i ti indeksi ce biti pozicije mina na osnovu kojih treba ispravno napraviti funkciju generateInitialState
+         random_list <- fmap (map (\x -> x `mod` 6)) $ fmap fst $ fmap (make_rs $ fields_num*fields_num) (fmap mkStdGen current_time)
+         --TODO treba iskoristiti
+         --(map (\x -> loadBMP $ "76px-Minesweeper_" ++ show x ++ "_svg.bmp") [0,1..8])
+         a0 <- loadBMP $ "76px-Minesweeper_0_svg.bmp"
+         a1 <- loadBMP $ "76px-Minesweeper_1_svg.bmp"
+         a2 <- loadBMP $ "76px-Minesweeper_2_svg.bmp"
+         a3 <- loadBMP $ "76px-Minesweeper_3_svg.bmp"
+         a4 <- loadBMP $ "76px-Minesweeper_4_svg.bmp"
+         a5 <- loadBMP $ "76px-Minesweeper_5_svg.bmp"
+         a6 <- loadBMP $ "76px-Minesweeper_6_svg.bmp"
+         a7 <- loadBMP $ "76px-Minesweeper_7_svg.bmp" 
+         a8 <- loadBMP $ "76px-Minesweeper_8_svg.bmp"
+         mina <- loadBMP "1473847111_large.bmp"
+         zastavica <- loadBMP "76px-Minesweeper_flag_svg.bmp"
+         precrtano <- loadBMP "1473847111_large_crossed.bmp"
+         let slike = [a0,a1,a2,a3,a4,a5,a6,a7,a8,mina,zastavica,precrtano]
+         let mines_matrix = generateMinesMatrix random_list
+         let initialState = generateInitialState mines_matrix fields_num mines_num
+         play mainWindow background fps initialState (render slike) handleKeys update
+           where
+             update :: Float -> GameState -> GameState
+             update _ = id
+
+             --funkcija koja vraca listu od n random brojeva i generator
+             --koristimo trenutno vrijeme za randomizaciju
+             --poziva se sa make_rs n (fmap mkStdGen current_time)
+             make_rs :: RandomGen g => Int -> g -> ([Int],g)
+             make_rs n g = loop [] n g
+               where
+                 loop acc 0 g = (reverse acc,g)
+                 loop acc n g = let (r,g') = randomR (0,n) g 
+                                in loop (r:acc) (pred n) g'
+
+             generateMinesMatrix :: [Int] -> [Bool]
+             generateMinesMatrix r_list =
+               let
+                 --sljedeca linija pronalazi najmanji element od 0 do 5 sa frekvencijom bar mines_num, indeksi u random_list tog elementa bice pozicije mina
+                 --treba pojednostaviti sljedeci kod
+                 selectedElem = ( fst . head . dropWhile (\g -> snd g < mines_num) . map (\g -> (head g, length g)). group . sort) r_list
+
+                 --TODO
+                 --Uraditi ovo sa foldl
+                 minesBefore :: Int -> [(Bool, Int)] -> [(Bool, Int)]
+                 minesBefore _ []     = []
+                 minesBefore acc (x:xs) = if fst x == True then (fst x,acc+1+snd x) : minesBefore (acc+1) xs
+                                                           else (fst x, acc) : minesBefore acc xs
+
+                 matrix_many_mines = map (\x -> x == selectedElem) r_list
+
+                 minesBefore10 = minesBefore 0 $ zip matrix_many_mines (repeat 0)
+                 firstPart = map fst $ takeWhile (\x -> snd x < 10) $ minesBefore10
+                 lengthTaken = length firstPart
+               in take (lengthTaken+1) matrix_many_mines ++ take ((fields_num*fields_num)-lengthTaken-1) (repeat False)
+
+             --funkcija koja na pocetku generise igru, ona treba da postavi mine, pa postavi brojeve od 0-8 u ostala polja
+             --ona ce da postavi sva polja matrice za stanje igre na Uncovered
+             generateInitialState :: [Bool] -> Int -> Int -> GameState
+             generateInitialState m_matrix fields_n mines_n =
+               let fields_value = count_mines $ map (\x -> if fst x then Mine else Neighbours 0) $ zip m_matrix [0..]
+                   fields_state = map (\x -> Uncovered) fields_value 
+               in Game (fields_value, fields_state)
 
 
 
